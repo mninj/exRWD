b634d9e3e6371c05eed6c427afcf40dae94dc824
mninj <mninj@hanmail.net>
2019-09-25 10:34:31 +0900

    모바일 메인 매뉴 제어를 위한 자바스크립트

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,33 +1,47 @@
-var nav = $('.navigation');
-var btn = $('.btn-menubar');
-var menu = $('.menu');
-var lists = $('.menu-list');
-var items = $('.menu-item');
-
-items.addClass('icon-plus');
-items.attr('role', 'button');
-items.attr('aria-haspopup', 'true');
-items.attr('aria-pressed', 'false');
-items.attr('aria-expanded', 'false');
-
-
-//메인메뉴 제어버튼 클릭 시 메인메뉴가 보여지도록 클래스 토글
-btn.on('click', function(e) {
-  // 혹시 a 요소를 사용했을 때를 대비하여 a 요소의 기본 이벤트 취소
-  e.preventDefault();
-  nav.toggleClass('is-act');
-  if (nav.hasClass('is-act')) {
-    btn.attr('aria-label', '메인 메뉴 닫기');
-  } else {
-    btn.attr('aria-label', '메인 메뉴 열기');
-  }
+var viewport = window.matchMedia('(max-width:999px)');
+$(window).resize(function(){
+  location.reload();
 });
-
-// 키보드 초점이 메인메뉴 영역 안에 있을 경우 .is-act를 nav 요소에 추가 
-menu.on('focusin', function(){
-  nav.addClass('is-act');
-});
-// 키보드 초점이 메인메뉴 영역을 벗어날 경우 .is-act를 nav 요소에서 제거
-menu.on('focusout', function(){
+if(viewport.matches){
+  var nav = $('.navigation');
+  var btn = $('.btn-menubar');
+  var menu = $('.menu');
+  var lists = $('.menu-list');
+  var items = $('.menu-item');
+  
+  items.addClass('icon-plus');
+  items.attr('role', 'button');
+  items.attr('aria-haspopup', 'true');
+  items.attr('aria-pressed', 'false');
+  items.attr('aria-expanded', 'false');
+  
+  btn.on('click', function(e){
+    e.preventDefault();
+    nav.toggleClass('is-act');
+    if(nav.hasClass('is-act')){
+      btn.attr('aria-label', '메인메뉴 닫기');
+    }else{
+      btn.attr('aria-label', '메인메뉴 열기');
+    }
+  });
+  
+  items.on('click', function(e){
+    e.preventDefault();
+    lists.removeClass('menu-act');
+    $(this).parent().addClass('menu-act');
+    items.removeClass('icon-minus').addClass('icon-plus');
+    $(this).removeClass('icon-plus').addClass('icon-minus');
+    if(lists.hasClass('menu-act')){
+      $(this).attr('aria-pressed', 'true').attr('aria-expanded', 'true');
+    }else{
+      items.attr('aria-pressed', 'false').attr('aria-expanded', 'false');
+    }
+  });
+  
+  menu.on('focusin', function(){
+    nav.addClass('is-act');
+  });
+  menu.on('focusout', function(){
     nav.removeClass('is-act');
-});
+  });
+}
